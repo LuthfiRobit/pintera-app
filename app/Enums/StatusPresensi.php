eb2209ebf48b1562c44845bef9f5d7c965b15059
LuthfiRobit <luthfirobit@gmail.com>
2026-07-26 17:31:26 +0700

    feat: add StatusSesiPembelajaran and StatusPresensi enums

---
+++ app/Enums/StatusPresensi.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Enums;
+
+enum StatusPresensi: string
+{
+    case Hadir = 'hadir';
+    case Izin = 'izin';
+    case Sakit = 'sakit';
+    case Alpa = 'alpa';
+    case Terlambat = 'terlambat';
+
+    public function label(): string
+    {
+        return match ($this) {
+            self::Hadir => 'Hadir',
+            self::Izin => 'Izin',
+            self::Sakit => 'Sakit',
+            self::Alpa => 'Alpa',
+            self::Terlambat => 'Terlambat',
+        };
+    }
+}
