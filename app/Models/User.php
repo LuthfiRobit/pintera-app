aa69ec8e55a13263e4083600c2cc3deb1b44cb1a
LuthfiRobit <luthfirobit@gmail.com>
2026-07-31 17:32:54 +0700

    feat: add username and must_change_password to users, make email nullable

--- app/Models/User.php
+++ app/Models/User.php
@@ -25,9 +25,11 @@ class User extends Authenticatable
     protected $fillable = [
         'name',
         'email',
+        'username',
         'password',
         'lembaga_id',
         'is_active',
+        'must_change_password',
         'email_verified_at',
     ];
 
@@ -52,6 +54,7 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
             'is_active' => 'boolean',
+            'must_change_password' => 'boolean',
         ];
     }
 
