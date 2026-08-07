3bbedd0f1e79fb34021363d0e289a22cd5540091
LuthfiRobit <luthfirobit@gmail.com>
2026-08-07 18:56:16 +0700

    fix(kasus): hapus @push('scripts') agar tomSelectSiswa tereksekusi oleh Alpine

--- resources/views/kasus/create.blade.php
+++ resources/views/kasus/create.blade.php
@@ -101,37 +101,35 @@
         })->values();
     @endphp
 
-    @push('scripts')
-        <script>
-            function tomSelectSiswa() {
-                return {
-                    options: @json($siswaOptions),
-                    oldValue: @json(old('siswa_id', '')),
-                    tomSelect: null,
-                    init() {
-                        this.tomSelect = new TomSelect(this.$refs.selectElement, {
-                            valueField: 'id',
-                            labelField: 'nama',
-                            searchField: ['nama', 'nis', 'nisn'],
-                            options: this.options,
-                            items: this.oldValue ? [this.oldValue] : [],
-                            placeholder: 'Ketik nama, NIS, atau NISN...',
-                            maxOptions: 50,
-                            render: {
-                                option: function(data, escape) {
-                                    return `<div>
-                                                <span class="block font-semibold text-gray-900">${escape(data.nama)}</span>
-                                                <span class="block text-xs font-mono text-gray-500">NIS: ${escape(data.nis)} | NISN: ${escape(data.nisn)}</span>
-                                            </div>`;
-                                },
-                                item: function(data, escape) {
-                                    return `<div>${escape(data.nama)} <span class="text-gray-400 font-mono text-xs">(NIS: ${escape(data.nis)})</span></div>`;
-                                }
+    <script>
+        function tomSelectSiswa() {
+            return {
+                options: @json($siswaOptions),
+                oldValue: @json(old('siswa_id', '')),
+                tomSelect: null,
+                init() {
+                    this.tomSelect = new TomSelect(this.$refs.selectElement, {
+                        valueField: 'id',
+                        labelField: 'nama',
+                        searchField: ['nama', 'nis', 'nisn'],
+                        options: this.options,
+                        items: this.oldValue ? [this.oldValue] : [],
+                        placeholder: 'Ketik nama, NIS, atau NISN...',
+                        maxOptions: 50,
+                        render: {
+                            option: function(data, escape) {
+                                return `<div>
+                                            <span class="block font-semibold text-gray-900">${escape(data.nama)}</span>
+                                            <span class="block text-xs font-mono text-gray-500">NIS: ${escape(data.nis)} | NISN: ${escape(data.nisn)}</span>
+                                        </div>`;
                             },
-                        });
-                    }
+                            item: function(data, escape) {
+                                return `<div>${escape(data.nama)} <span class="text-gray-400 font-mono text-xs">(NIS: ${escape(data.nis)})</span></div>`;
+                            }
+                        },
+                    });
                 }
             }
-        </script>
-    @endpush
+        }
+    </script>
 </x-app-layout>
