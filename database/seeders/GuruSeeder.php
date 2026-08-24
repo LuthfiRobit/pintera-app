35f9c87a0265962a4482b46d5e335a107215eb0c
LuthfiRobit <luthfirobit@gmail.com>
2026-08-24 21:51:37 +0700

    refactor(seeder): retarget GuruSeeder+UserSeeder ke SD, tambah 12 wali kelas (total 15 guru)

--- database/seeders/GuruSeeder.php
+++ database/seeders/GuruSeeder.php
@@ -12,83 +12,98 @@ class GuruSeeder extends Seeder
 {
     public function run(): void
     {
-        $kbit = Lembaga::where('npsn', '20223311')->firstOrFail();
-        $tkit = Lembaga::where('npsn', '20223322')->firstOrFail();
         $sdit = Lembaga::where('npsn', '20223333')->firstOrFail();
-        $smpit = Lembaga::where('npsn', '20223344')->firstOrFail();
 
-        $this->seedGuru($kbit, [
+        // 12 wali kelas (guru_kelas) -- 1 per rombel, 2 rombel x 6 tingkat
+        $this->seedGuru($sdit, [
             [
-                'email' => 'guru.kb1@demo.test', 'name' => 'Fatimah, S.Psi.',
-                'nik' => '3273011101850011', 'nuptk' => '3234567890123411', 'nip' => null,
-                'jenis_kelamin' => 'P', 'tempat_lahir' => 'Probolinggo', 'tanggal_lahir' => '1989-05-12',
-                'no_hp' => '081234567811', 'jenis_ptk' => 'guru_kelas', 'status_kepegawaian' => 'GTY',
-                'golongan_pangkat' => null, 'tmt_tugas' => '2018-07-01', 'tmt_pns' => null,
-            ],
-            [
-                'email' => 'guru.kb2@demo.test', 'name' => 'Zahra, S.Pd.',
-                'nik' => '3273011202860012', 'nuptk' => '3234567890123412', 'nip' => null,
-                'jenis_kelamin' => 'P', 'tempat_lahir' => 'Probolinggo', 'tanggal_lahir' => '1990-08-15',
-                'no_hp' => '081234567812', 'jenis_ptk' => 'guru_kelas', 'status_kepegawaian' => 'GTY',
-                'golongan_pangkat' => null, 'tmt_tugas' => '2019-07-01', 'tmt_pns' => null,
-            ],
-            [
-                'email' => 'guru.kb3@demo.test', 'name' => 'Rini, S.Pd.',
-                'nik' => '3273011303870013', 'nuptk' => null, 'nip' => null,
-                'jenis_kelamin' => 'P', 'tempat_lahir' => 'Surabaja', 'tanggal_lahir' => '1992-03-20',
-                'no_hp' => '081234567813', 'jenis_ptk' => 'guru_kelas', 'status_kepegawaian' => 'Honorer',
-                'golongan_pangkat' => null, 'tmt_tugas' => '2020-07-01', 'tmt_pns' => null,
-            ],
-        ]);
-
-        $this->seedGuru($tkit, [
-            [
-                'email' => 'guru.tk1@demo.test', 'name' => 'Dewi, S.Pd.I.',
-                'nik' => '3273012101850021', 'nuptk' => '4234567890123421', 'nip' => null,
-                'jenis_kelamin' => 'P', 'tempat_lahir' => 'Malang', 'tanggal_lahir' => '1988-04-10',
-                'no_hp' => '081234567821', 'jenis_ptk' => 'guru_kelas', 'status_kepegawaian' => 'GTY',
-                'golongan_pangkat' => null, 'tmt_tugas' => '2017-07-01', 'tmt_pns' => null,
-            ],
-            [
-                'email' => 'guru.tk2@demo.test', 'name' => 'Latifah, S.Pd.',
-                'nik' => '3273012202860022', 'nuptk' => '4234567890123422', 'nip' => null,
-                'jenis_kelamin' => 'P', 'tempat_lahir' => 'Pasuruan', 'tanggal_lahir' => '1991-09-11',
-                'no_hp' => '081234567822', 'jenis_ptk' => 'guru_kelas', 'status_kepegawaian' => 'GTY',
-                'golongan_pangkat' => null, 'tmt_tugas' => '2018-07-01', 'tmt_pns' => null,
-            ],
-            [
-                'email' => 'guru.tk3@demo.test', 'name' => 'Amel, S.Psi.',
-                'nik' => '3273012303870023', 'nuptk' => null, 'nip' => null,
-                'jenis_kelamin' => 'P', 'tempat_lahir' => 'Jember', 'tanggal_lahir' => '1993-11-25',
-                'no_hp' => '081234567823', 'jenis_ptk' => 'guru_kelas', 'status_kepegawaian' => 'Honorer',
-                'golongan_pangkat' => null, 'tmt_tugas' => '2021-07-01', 'tmt_pns' => null,
-            ],
-        ]);
-
-        $this->seedGuru($smpit, [
-            [
-                'email' => 'budi.santoso@demo.test', 'name' => 'Budi Santoso, S.Pd.',
-                'nik' => '3273011503850001', 'nuptk' => '1234567890123456', 'nip' => '198503152010011001',
-                'jenis_kelamin' => 'L', 'tempat_lahir' => 'Bandung', 'tanggal_lahir' => '1985-03-15',
-                'no_hp' => '081234567801', 'jenis_ptk' => 'guru_mapel', 'status_kepegawaian' => 'PNS',
-                'golongan_pangkat' => 'Penata Muda Tk.I / III-b', 'tmt_tugas' => '2010-01-01', 'tmt_pns' => '2010-01-01',
-            ],
-            [
-                'email' => 'siti.rahmawati@demo.test', 'name' => 'Siti Rahmawati, S.Pd.',
-                'nik' => '3273015207880002', 'nuptk' => null, 'nip' => null,
-                'jenis_kelamin' => 'P', 'tempat_lahir' => 'Cimahi', 'tanggal_lahir' => '1988-07-12',
-                'no_hp' => '081234567802', 'jenis_ptk' => 'guru_kelas', 'status_kepegawaian' => 'GTY',
+                'email' => 'sari.wulandari@demo.test', 'name' => 'Sari Wulandari, S.Pd.',
+                'nik' => '3273011101880101', 'nuptk' => '5234567890123401', 'nip' => null,
+                'jenis_kelamin' => 'P', 'tempat_lahir' => 'Probolinggo', 'tanggal_lahir' => '1988-02-14',
+                'no_hp' => '081234568101', 'jenis_ptk' => 'guru_kelas', 'status_kepegawaian' => 'GTY',
                 'golongan_pangkat' => null, 'tmt_tugas' => '2015-07-01', 'tmt_pns' => null,
             ],
             [
-                'email' => 'andi.wijaya@demo.test', 'name' => 'Andi Wijaya, S.Pd.I.',
-                'nik' => '3273012009900003', 'nuptk' => null, 'nip' => null,
-                'jenis_kelamin' => 'L', 'tempat_lahir' => 'Garut', 'tanggal_lahir' => '1990-09-20',
-                'no_hp' => '081234567803', 'jenis_ptk' => 'guru_mapel', 'status_kepegawaian' => 'Honorer',
+                'email' => 'agus.setiawan@demo.test', 'name' => 'Agus Setiawan, S.Pd.',
+                'nik' => '3273011203870102', 'nuptk' => '5234567890123402', 'nip' => null,
+                'jenis_kelamin' => 'L', 'tempat_lahir' => 'Kraksaan', 'tanggal_lahir' => '1987-05-20',
+                'no_hp' => '081234568102', 'jenis_ptk' => 'guru_kelas', 'status_kepegawaian' => 'GTY',
+                'golongan_pangkat' => null, 'tmt_tugas' => '2016-07-01', 'tmt_pns' => null,
+            ],
+            [
+                'email' => 'nita.kurniawati@demo.test', 'name' => 'Nita Kurniawati, S.Pd.',
+                'nik' => '3273011304890103', 'nuptk' => null, 'nip' => null,
+                'jenis_kelamin' => 'P', 'tempat_lahir' => 'Probolinggo', 'tanggal_lahir' => '1989-08-11',
+                'no_hp' => '081234568103', 'jenis_ptk' => 'guru_kelas', 'status_kepegawaian' => 'Honorer',
                 'golongan_pangkat' => null, 'tmt_tugas' => '2019-07-01', 'tmt_pns' => null,
+            ],
+            [
+                'email' => 'rudi.hartono@demo.test', 'name' => 'Rudi Hartono, S.Pd.',
+                'nik' => '3273011405860104', 'nuptk' => '5234567890123404', 'nip' => '198605042009011010',
+                'jenis_kelamin' => 'L', 'tempat_lahir' => 'Jember', 'tanggal_lahir' => '1986-05-04',
+                'no_hp' => '081234568104', 'jenis_ptk' => 'guru_kelas', 'status_kepegawaian' => 'PNS',
+                'golongan_pangkat' => 'Penata Muda / III-a', 'tmt_tugas' => '2009-01-01', 'tmt_pns' => '2009-01-01',
+            ],
+            [
+                'email' => 'wahyu.astuti@demo.test', 'name' => 'Wahyu Astuti, S.Pd.',
+                'nik' => '3273011506910105', 'nuptk' => null, 'nip' => null,
+                'jenis_kelamin' => 'P', 'tempat_lahir' => 'Malang', 'tanggal_lahir' => '1991-06-15',
+                'no_hp' => '081234568105', 'jenis_ptk' => 'guru_kelas', 'status_kepegawaian' => 'PPPK',
+                'golongan_pangkat' => 'IX', 'tmt_tugas' => '2020-03-01', 'tmt_pns' => null,
+            ],
+            [
+                'email' => 'dedi.iskandar@demo.test', 'name' => 'Dedi Iskandar, S.Pd.',
+                'nik' => '3273011607850106', 'nuptk' => '5234567890123406', 'nip' => null,
+                'jenis_kelamin' => 'L', 'tempat_lahir' => 'Probolinggo', 'tanggal_lahir' => '1985-07-19',
+                'no_hp' => '081234568106', 'jenis_ptk' => 'guru_kelas', 'status_kepegawaian' => 'GTY',
+                'golongan_pangkat' => null, 'tmt_tugas' => '2014-07-01', 'tmt_pns' => null,
+            ],
+            [
+                'email' => 'fitriani.rahmawati@demo.test', 'name' => 'Fitriani Rahmawati, S.Pd.',
+                'nik' => '3273011708920107', 'nuptk' => null, 'nip' => null,
+                'jenis_kelamin' => 'P', 'tempat_lahir' => 'Bondowoso', 'tanggal_lahir' => '1992-08-23',
+                'no_hp' => '081234568107', 'jenis_ptk' => 'guru_kelas', 'status_kepegawaian' => 'Honorer',
+                'golongan_pangkat' => null, 'tmt_tugas' => '2021-07-01', 'tmt_pns' => null,
+            ],
+            [
+                'email' => 'bambang.wijaya@demo.test', 'name' => 'Bambang Wijaya, S.Pd.',
+                'nik' => '3273011809840108', 'nuptk' => '5234567890123408', 'nip' => '198409082008011008',
+                'jenis_kelamin' => 'L', 'tempat_lahir' => 'Situbondo', 'tanggal_lahir' => '1984-09-08',
+                'no_hp' => '081234568108', 'jenis_ptk' => 'guru_kelas', 'status_kepegawaian' => 'PNS',
+                'golongan_pangkat' => 'Penata / III-c', 'tmt_tugas' => '2008-01-01', 'tmt_pns' => '2008-01-01',
+            ],
+            [
+                'email' => 'ratna.puspita@demo.test', 'name' => 'Ratna Puspita, S.Pd.',
+                'nik' => '3273011910930109', 'nuptk' => null, 'nip' => null,
+                'jenis_kelamin' => 'P', 'tempat_lahir' => 'Probolinggo', 'tanggal_lahir' => '1993-10-30',
+                'no_hp' => '081234568109', 'jenis_ptk' => 'guru_kelas', 'status_kepegawaian' => 'Honorer',
+                'golongan_pangkat' => null, 'tmt_tugas' => '2022-07-01', 'tmt_pns' => null,
+            ],
+            [
+                'email' => 'yusuf.maulana@demo.test', 'name' => 'Yusuf Maulana, S.Pd.',
+                'nik' => '3273012011890110', 'nuptk' => '5234567890123410', 'nip' => null,
+                'jenis_kelamin' => 'L', 'tempat_lahir' => 'Kraksaan', 'tanggal_lahir' => '1989-11-05',
+                'no_hp' => '081234568110', 'jenis_ptk' => 'guru_kelas', 'status_kepegawaian' => 'GTY',
+                'golongan_pangkat' => null, 'tmt_tugas' => '2017-07-01', 'tmt_pns' => null,
+            ],
+            [
+                'email' => 'lina.marlina@demo.test', 'name' => 'Lina Marlina, S.Pd.',
+                'nik' => '3273012112870111', 'nuptk' => null, 'nip' => null,
+                'jenis_kelamin' => 'P', 'tempat_lahir' => 'Probolinggo', 'tanggal_lahir' => '1987-12-12',
+                'no_hp' => '081234568111', 'jenis_ptk' => 'guru_kelas', 'status_kepegawaian' => 'GTY',
+                'golongan_pangkat' => null, 'tmt_tugas' => '2013-07-01', 'tmt_pns' => null,
+            ],
+            [
+                'email' => 'irfan.hakim@demo.test', 'name' => 'Irfan Hakim, S.Pd.',
+                'nik' => '3273012213900112', 'nuptk' => '5234567890123412', 'nip' => null,
+                'jenis_kelamin' => 'L', 'tempat_lahir' => 'Lumajang', 'tanggal_lahir' => '1990-01-13',
+                'no_hp' => '081234568112', 'jenis_ptk' => 'guru_kelas', 'status_kepegawaian' => 'PPPK',
+                'golongan_pangkat' => 'VIII', 'tmt_tugas' => '2020-08-01', 'tmt_pns' => null,
             ],
         ]);
 
+        // 3 guru mapel spesialis (existing, dipertahankan persis) + 1 diangkat jadi guru_bk
+        // (Task 6 mengangkat hendra.gunawan sbg guru_bk pengganti budi.santoso SMP yang dihapus)
         $this->seedGuru($sdit, [
             [
                 'email' => 'hendra.gunawan@demo.test', 'name' => 'Hendra Gunawan, S.Pd.',
@@ -138,14 +153,14 @@ class GuruSeeder extends Seeder
                     'tanggal_lahir' => $data['tanggal_lahir'],
                     'agama' => 'Islam',
                     'kewarganegaraan' => 'WNI',
-                    'alamat_jalan' => 'Jl. Cihampelas No. 125',
-                    'rt' => '002',
-                    'rw' => '005',
-                    'desa_kelurahan' => 'Cipaganti',
-                    'kecamatan' => 'Coblong',
-                    'kabupaten_kota' => 'Kota Bandung',
-                    'provinsi' => 'Jawa Barat',
-                    'kode_pos' => '40131',
+                    'alamat_jalan' => 'Jl. Panglima Sudirman No. 88C',
+                    'rt' => '001',
+                    'rw' => '003',
+                    'desa_kelurahan' => 'Sidomulyo',
+                    'kecamatan' => 'Kraksaan',
+                    'kabupaten_kota' => 'Kabupaten Probolinggo',
+                    'provinsi' => 'Jawa Timur',
+                    'kode_pos' => '67282',
                     'no_hp' => $data['no_hp'],
                     'email' => $data['email'],
                     'jenis_ptk' => $data['jenis_ptk'],
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\Lembaga;
 use App\Models\User;
+use App\Models\Yayasan;
 use Illuminate\Database\Seeder;
 
 class UserSeeder extends Seeder
@@ -16,66 +17,45 @@ class UserSeeder extends Seeder
             return;
         }
 
-        $kbit = Lembaga::where('npsn', '20223311')->firstOrFail();
+        // Admin yayasan scope-nya yayasan, TIDAK bergantung pada lembaga manapun -- sebelumnya
+        // salah query ke Lembaga NPSN KB yang sekarang sudah dihapus.
+        $yayasan = Yayasan::firstOrFail();
 
         if (! User::where('email', 'adm.yayasan@demo.test')->exists()) {
             $adminYayasan = User::create([
                 'name' => 'Ahmad Fauzi (Admin Yayasan)',
                 'email' => 'adm.yayasan@demo.test',
                 'password' => 'password',
-                'yayasan_id' => $kbit->yayasan_id,
+                'yayasan_id' => $yayasan->id,
                 'email_verified_at' => now(),
                 'is_active' => true,
             ]);
             $adminYayasan->assignRole('yayasan_super_admin');
         }
 
-        $tkit = Lembaga::where('npsn', '20223322')->firstOrFail();
         $sdit = Lembaga::where('npsn', '20223333')->firstOrFail();
-        $smpit = Lembaga::where('npsn', '20223344')->firstOrFail();
 
-        // KBIT
-        $this->seedStaf($kbit, [
-            ['name' => 'Aisyah, S.Psi.', 'email' => 'kepsek.kb@demo.test', 'role' => 'kepala_sekolah'],
-            ['name' => 'Nurul, S.Pd.', 'email' => 'adm.kb@demo.test', 'role' => 'admin_administrasi'],
-            ['name' => 'Halimah, S.E.', 'email' => 'keuangan.kb@demo.test', 'role' => 'admin_keuangan'],
-        ], [
-            ['name' => 'Fatimah, S.Psi.', 'email' => 'guru.kb1@demo.test'],
-            ['name' => 'Zahra, S.Pd.', 'email' => 'guru.kb2@demo.test'],
-            ['name' => 'Rini, S.Pd.', 'email' => 'guru.kb3@demo.test'],
-        ]);
-
-        // TKIT
-        $this->seedStaf($tkit, [
-            ['name' => 'Maryam, S.Pd.I.', 'email' => 'kepsek.tk@demo.test', 'role' => 'kepala_sekolah'],
-            ['name' => 'Indria, S.Pd.', 'email' => 'adm.tk@demo.test', 'role' => 'admin_administrasi'],
-            ['name' => 'Khadijah, S.E.', 'email' => 'keuangan.tk@demo.test', 'role' => 'admin_keuangan'],
-        ], [
-            ['name' => 'Dewi, S.Pd.I.', 'email' => 'guru.tk1@demo.test'],
-            ['name' => 'Latifah, S.Pd.', 'email' => 'guru.tk2@demo.test'],
-            ['name' => 'Amel, S.Psi.', 'email' => 'guru.tk3@demo.test'],
-        ]);
-
-        // SDIT
+        // SDIT -- pimpinan + 12 wali kelas + 3 guru mapel spesialis (total 15 guru)
         $this->seedStaf($sdit, [
             ['name' => 'Abdullah, M.Pd.', 'email' => 'kepsek.sd@demo.test', 'role' => 'kepala_sekolah'],
             ['name' => 'Lukman, S.Kom.', 'email' => 'adm.sd@demo.test', 'role' => 'admin_administrasi'],
             ['name' => 'Hasan, S.E.', 'email' => 'keuangan.sd@demo.test', 'role' => 'admin_keuangan'],
         ], [
+            ['name' => 'Sari Wulandari, S.Pd.', 'email' => 'sari.wulandari@demo.test'],
+            ['name' => 'Agus Setiawan, S.Pd.', 'email' => 'agus.setiawan@demo.test'],
+            ['name' => 'Nita Kurniawati, S.Pd.', 'email' => 'nita.kurniawati@demo.test'],
+            ['name' => 'Rudi Hartono, S.Pd.', 'email' => 'rudi.hartono@demo.test'],
+            ['name' => 'Wahyu Astuti, S.Pd.', 'email' => 'wahyu.astuti@demo.test'],
+            ['name' => 'Dedi Iskandar, S.Pd.', 'email' => 'dedi.iskandar@demo.test'],
+            ['name' => 'Fitriani Rahmawati, S.Pd.', 'email' => 'fitriani.rahmawati@demo.test'],
+            ['name' => 'Bambang Wijaya, S.Pd.', 'email' => 'bambang.wijaya@demo.test'],
+            ['name' => 'Ratna Puspita, S.Pd.', 'email' => 'ratna.puspita@demo.test'],
+            ['name' => 'Yusuf Maulana, S.Pd.', 'email' => 'yusuf.maulana@demo.test'],
+            ['name' => 'Lina Marlina, S.Pd.', 'email' => 'lina.marlina@demo.test'],
+            ['name' => 'Irfan Hakim, S.Pd.', 'email' => 'irfan.hakim@demo.test'],
             ['name' => 'Hendra Gunawan, S.Pd.', 'email' => 'hendra.gunawan@demo.test'],
             ['name' => 'Maya Anggraini, S.Pd.', 'email' => 'maya.anggraini@demo.test'],
             ['name' => 'Taufik Hidayat, S.Pd.', 'email' => 'taufik.hidayat@demo.test'],
-        ]);
-
-        // SMPIT
-        $this->seedStaf($smpit, [
-            ['name' => 'Bambang Suryadi, M.Pd.', 'email' => 'kepsek.smp@demo.test', 'role' => 'kepala_sekolah'],
-            ['name' => 'Dewi Lestari, S.Pd.', 'email' => 'adm.smp@demo.test', 'role' => 'admin_administrasi'],
-            ['name' => 'Nur Aisyah, S.Pd.', 'email' => 'keuangan.smp@demo.test', 'role' => 'admin_keuangan'],
-        ], [
-            ['name' => 'Budi Santoso, S.Pd.', 'email' => 'budi.santoso@demo.test'],
-            ['name' => 'Siti Rahmawati, S.Pd.', 'email' => 'siti.rahmawati@demo.test'],
-            ['name' => 'Andi Wijaya, S.Pd.I.', 'email' => 'andi.wijaya@demo.test'],
         ]);
     }
 
