7f3a0560bbdde0cfc813d36878709c50afe42a9d
LuthfiRobit <luthfirobit@gmail.com>
2026-08-24 08:22:01 +0700

    refactor(keuangan): cross-scope touch ReconcilePayments dan BriTestQris command (namespace model/gateway baru)

--- database/factories/BriVirtualAccountFactory.php
+++ database/factories/BriVirtualAccountFactory.php
@@ -10,6 +10,7 @@ use Illuminate\Database\Eloquent\Factories\Factory;
  */
 class BriVirtualAccountFactory extends Factory
 {
+    protected $model = BriVirtualAccount::class;
     /**
      * Define the model's default state.
      *
